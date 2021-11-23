5cfff63d383e4ea976b9d118a279d2bfc8c113d1
unsubstantiated-Script <jusfulton@gmail.com>
2021-11-23 12:25:30 -0800

    components basics done

--- vueComponents/app.js
+++ vueComponents/app.js
@@ -21,7 +21,33 @@ const app = Vue.createApp({
 					email: "karate@gmail.com",
 				},
 			],
+		};
+	},
+});
+
+//Vue components are apps within apps => name, component
+app.component("friend-contact", {
+	template: `	
+    <li>
+		<h2>{{friend.name}}</h2>
+			<button @click="toggleDetails">
+				{{detailsVisible ? "Hide" : "Show"}} Details
+			</button>
+		<ul v-if="detailsVisible">
+			<li><strong>Phone:</strong> {{friend.phone}}</li>
+			<li><strong>Email:</strong> {{friend.email}}</li>
+		</ul>
+	</li>
+                `,
+	data() {
+		return {
 			detailsVisible: false,
+			friend: {
+				id: 3,
+				name: "Fuki Kata Itch",
+				phone: "555-554-5543",
+				email: "karate@gmail.com",
+			},
 		};
 	},
 	methods: {
